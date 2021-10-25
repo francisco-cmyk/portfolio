e69af1e60169a76bc96a43d989ec73700d77c031
Francisco <fveranicola@gmail.com>
2021-10-25 14:03:56 -0400

    Downloaded and connected SCSS plugin

--- portfolio-page/src/pages/index.js
+++ portfolio-page/src/pages/index.js
@@ -6,32 +6,13 @@ import * as React from "react"
 // import Seo from "../components/seo"
 
 const IndexPage = () => (
-  <div>
+  <div className="home-outter-container">
+    <div className="home-inner-container">
     <h3>Home</h3>
     <p>hey</p>
+    <p>YOOO THIS GONNA BE A SICK PORTFOLIO PAGE YUH</p>
+    </div>
   </div>
 )
 
 export default IndexPage
-
-
-// <Layout>
-// <Seo title="Home" />
-// <h1>Hi people</h1>
-// <p>Welcome to my website</p>
-// <p>Now go build something great.</p>
-// <StaticImage
-//   src="../images/gatsby-astronaut.png"
-//   width={300}
-//   quality={95}
-//   formats={["auto", "webp", "avif"]}
-//   alt="A Gatsby astronaut"
-//   style={{ marginBottom: `1.45rem` }}
-// />
-// <p>
-//   <Link to="/page-2/">Go to page 2</Link> <br />
-//   <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-//   <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-//   <Link to="/using-dsg">Go to "Using DSG"</Link>
-// </p>
-// </Layout>
