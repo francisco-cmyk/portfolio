cceccd9489a5ee468502b44108f2ecb055a18ece
Francisco <fveranicola@gmail.com>
2021-11-06 21:02:36 -0400

    Added mobile navbar functionality and added grid to tech stack

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,23 @@
 import { Link } from "gatsby"
-import React from "react";
-
+import React, { useState } from "react";
+// import { motion } from "framer-motion"
+import { GiHamburgerMenu } from "react-icons/gi";
 function Navbar() {
+  const [show, setShow] = useState(false);
+  const mobileNav = () => {
+    setShow(!show);
+  }
   return (
     <div className="navbar-outter-container">
+      <div className="mobile-navbar-icon">
+        <GiHamburgerMenu className="mobile-icon" onClick={() => mobileNav()} />
+        {show ? (<div className="mobile-nav-links-container">
+          <Link onClick={() => mobileNav()} className="mobile-link" to="/#home">Home</Link>
+          <Link onClick={() => mobileNav()} className="mobile-link" to="/#about">About Me</Link>
+          <Link onClick={() => mobileNav()} className="mobile-link" to="/#projects">Projects</Link>
+          <Link onClick={() => mobileNav()} className="mobile-link" to="/#contact">Contact</Link>
+        </div>) : null}
+      </div>
       <div className="navbar-inner-container">
         <Link className="navbar-link" to="/#home">Home</Link>
         <Link className="navbar-link" to="/#about">About Me</Link>
